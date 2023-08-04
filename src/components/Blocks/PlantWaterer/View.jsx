d6d165651c211657c1ea802d314eb507b6892fde
fosten <fosten@gmail.com>
2023-08-04 19:27:04 -0400

    lint code prettier

--- src/components/Blocks/PlantWaterer/View.jsx
+++ src/components/Blocks/PlantWaterer/View.jsx
@@ -21,20 +21,18 @@ const View = (props) => {
       `token`,
       JSON.stringify(window.env.RAZZLE_HOMEASSISTANT_ACCESS_TOKEN),
     );
-      try {
-        const response = await axios.get(
-            `${window.env.RAZZLE_HOMEASSISTANT_API_HOST}:${window.env.RAZZLE_HOMEASSISTANT_API_PORT}/api/states`,
-            {
-              headers: {
-                Authorization: `Bearer ${window.env.RAZZLE_HOMEASSISTANT_ACCESS_TOKEN}`,
-                "Content-Type": "application/json"              },
-            },
-          );
-        setState(response);
-        window.localStorage.setItem(
-            `response`,
-            JSON.stringify(response),
-          );
+    try {
+      const response = await axios.get(
+        `${window.env.RAZZLE_HOMEASSISTANT_API_HOST}:${window.env.RAZZLE_HOMEASSISTANT_API_PORT}/api/states`,
+        {
+          headers: {
+            Authorization: `Bearer ${window.env.RAZZLE_HOMEASSISTANT_ACCESS_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      setState(response);
+      window.localStorage.setItem(`response`, JSON.stringify(response));
     } catch (err) {
       // eslint-disable-next-line no-console
       console.log(err);
@@ -47,47 +45,49 @@ const View = (props) => {
 
   const renderthis = () => {
     return (
-        <div className="container">
-          <div className="plantwatererinfo">
+      <div className="container">
+        <div className="plantwatererinfo">
           <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <th>Sensor</th>
-              <th>State</th>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-          <h2>Soil Moisture Sensors</h2>
-          {response.data
+            <Table.Header>
+              <Table.Row>
+                <th>Sensor</th>
+                <th>State</th>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <h2>Soil Moisture Sensors</h2>
+              {response.data
                 ?.filter((plantwaterer) =>
                   plantwaterer.entity_id.includes('sensor.soil_moisture'),
-                ).map((item, i) => {
-              return (
-                <tr key={i}>
-                  <td>{item.entity_id}</td>
-                  <td>{item.state}</td>
-                </tr>
-              );
-            })}
-            <h2>Water Pump Relays</h2>
-            {response.data
+                )
+                .map((item, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{item.entity_id}</td>
+                      <td>{item.state}</td>
+                    </tr>
+                  );
+                })}
+              <h2>Water Pump Relays</h2>
+              {response.data
                 ?.filter((plantwaterer) =>
                   plantwaterer.entity_id.includes('sensor.water_pump'),
-                ).map((item, i) => {
-              return (
-                <tr key={i}>
-                  <td>{item.entity_id}</td>
-                  <td>{item.state}</td>
-                </tr>
-              );
-            })}
-          </Table.Body>
-        </Table>
-          </div>
+                )
+                .map((item, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{item.entity_id}</td>
+                      <td>{item.state}</td>
+                    </tr>
+                  );
+                })}
+            </Table.Body>
+          </Table>
         </div>
-      );
-    };
-    
+      </div>
+    );
+  };
+
   var yoyo = renderthis();
   return yoyo;
 };
