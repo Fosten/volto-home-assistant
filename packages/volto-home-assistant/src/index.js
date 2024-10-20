1e72b335ba1dd72a54a6f3dfa70e7f924afa7ab0
Fosten <fosten@gmail.com>
2024-10-19 22:45:56 -0400

    Add axios dependency, move index.js, i18n

--- packages/volto-home-assistant/src/index.js
+++ packages/volto-home-assistant/src/index.js
@@ -1,4 +1,14 @@
+import '@plone/volto/config';
+import customBlocks from './components/Blocks/customBlocks';
+
 const applyConfig = (config) => {
+  config.blocks = {
+    ...config.blocks,
+    blocksConfig: {
+      ...config.blocks.blocksConfig,
+      ...customBlocks,
+    },
+  };
   return config;
 };
 
